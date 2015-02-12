c737719ea25c5965080f268d5d3a56768203d943
Russell Tan <sell_nat@yahoo.com>
2015-02-11 17:47:45 -0800

    SWAG

--- HelloGit/Source Files/HelloWorld.java
+++ HelloGit/Source Files/HelloWorld.java
@@ -2,7 +2,7 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Life Team test 2!!!");
+		System.out.println("Hello Life Team test 2 swag!!!");
 		System.out.println("Git testing yo momma!");
 		
 
